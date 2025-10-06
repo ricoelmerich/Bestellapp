e1e0a7876975993a8b0dcfca7bebc4494b452c6a
rico <rico-elmerich@t-online.de>
2025-10-06 14:42:48 +0200

    -Bilder für Buttons im Warenkorb eingefügt
    -Breite der Kategorie Bilder angepasst

--- scripts/template.js
+++ scripts/template.js
@@ -19,6 +19,7 @@ function dishTemplate(dishesIndex) {
 
 function selectedDishTemplate (item){
     return `<div class="selectedDish">${item.name}, ${item.price}€
-    <span class="quantity">x${item.quantity}<button onclick="decreaseQuantity(${item.index})">-</button><button onclick="increaseQuantity(${item.index})">+</button></span>
+    <span class="quantity">x${item.quantity}<button onclick="decreaseQuantity(${item.index})"><img src="assets/favicon/minus-1270000_640.png" class="basket-quantity-button"></button>
+    <button onclick="increaseQuantity(${item.index})"><img src="assets/favicon/+.png" class="basket-quantity-button"></button></span>
     </div>`
 }
