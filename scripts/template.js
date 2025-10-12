cf811656bc3a828fc2fd40669bf1f2064a2d90f2
rico <rico-elmerich@t-online.de>
2025-10-12 23:52:40 +0200

    -Home Link eingefügt -2 Nachkommastellen für Warenkorbpreise vorgegeben

--- scripts/template.js
+++ scripts/template.js
@@ -9,7 +9,7 @@ function dishTemplate(dishesIndex) {
     return`<div class="dishes">
     <div class="dishes-info">
     <h3>${myDishes[dishesIndex].name} </h3>
-    <br> ${myDishes[dishesIndex].price}€
+    <br> ${myDishes[dishesIndex].price.toFixed(2)} €
     <p>${myDishes[dishesIndex].description}</p>
     </div>
     <button class="add-button" onclick="addToBasket(${dishesIndex})"><img src="assets/favicon/+.png" class="add-button-img" alt=""></button>
@@ -18,7 +18,7 @@ function dishTemplate(dishesIndex) {
 }
 
 function selectedDishTemplate (item){
-    return `<div class="selectedDish">${item.name}, ${item.price.toFixed(2)}€
+    return `<div class="selectedDish">${item.name} ${item.price.toFixed(2)} €
     <span class="quantity">x${item.quantity}<button onclick="decreaseQuantity(${item.index})"><img src="assets/favicon/minus-1270000_640.png" class="basket-quantity-button"></button>
     <button onclick="increaseQuantity(${item.index})"><img src="assets/favicon/+.png" class="basket-quantity-button"></button></span>
     </div>`
